48c80f2f09e6ca2bc43be958d8e9d02dbd642e23
danrayfr <danrayrollan98@gmail.com>
2024-02-15 00:48:17 +0800

    unpin/pin el transition

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,5 +6,6 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
-import NotificationController from "./notification_controller"
-application.register("notification", NotificationController)
+// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
+// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
+// lazyLoadControllersFrom("controllers", application)
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin "trix"
-pin "@rails/actiontext", to: "actiontext.esm.js"
-pin "el-transition" # @0.0.7
+pin 'application'
+pin '@hotwired/turbo-rails', to: 'turbo.min.js'
+pin '@hotwired/stimulus', to: 'stimulus.min.js'
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
+pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin 'trix'
+pin '@rails/actiontext', to: 'actiontext.esm.js'
+pin 'el-transition', to: 'https://ga.jspm.io/npm:el-transition@0.0.7/index.js'
