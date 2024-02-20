2b0d664b8565ffe64266e835973d9643e232c41c
danrayfr <danrayrollan98@gmail.com>
2024-02-21 03:51:30 +0800

    Authenticate user in progress controller

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -1,4 +1,5 @@
 class ProgressesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_progress, only: %i[show edit update destroy]
 
   # GET /progresses or /progresses.json
