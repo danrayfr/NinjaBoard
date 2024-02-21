28509076d7e66d2a65dc8250c218189d61273911
danrayfr <danrayrollan98@gmail.com>
2024-02-22 06:05:24 +0800

    Responsive dropdown

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import Dropdown from 'stimulus-dropdown'
 
 export default class extends Dropdown {
-  static targets = ["menu", "avatar"];
+  static targets = ["menu", "avatarMenu", "chevron"];
 
   connect() {
     super.connect()
@@ -17,12 +17,14 @@ export default class extends Dropdown {
   }
 
   toggleAvatarDropdown() {
-    if (this.avatarTarget.classList.contains('hidden')) {
-      this.avatarTarget.classList.remove('hidden');
-      this.avatarTarget.classList.add('block');
+    if (this.avatarMenuTarget.style.display === 'none') {
+      this.avatarMenuTarget.style.display = 'block';
+      this.chevronTarget.classList.remove('rotate-0');
+      this.chevronTarget.classList.add('rotate-180');
     } else {
-      this.avatarTarget.classList.remove('block');
-      this.avatarTarget.classList.add('hidden');
+      this.avatarMenuTarget.style.display = 'none';
+      this.chevronTarget.classList.remove('rotate-180');
+      this.chevronTarget.classList.add('rotate-0');
     }
   }
 }
