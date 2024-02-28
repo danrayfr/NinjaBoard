de084e43698c50eb826970b181ccc2f8b326ab2b
danrayfr <danrayrollan98@gmail.com>
2024-02-28 17:27:39 +0800

    Update certificate index page, certificate & trophies tab

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -3,7 +3,7 @@
 class Certificate < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence: true, uniqueness: true, length: { maximum: 100 }
+  validates :title, presence: true, length: { maximum: 100 }
   enum source: %i[default linkedin udemy coursera]
   validates :date_awarded, presence: true
 
