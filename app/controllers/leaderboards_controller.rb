d373d19ee84df058ad977789deab0c29e75a3236
danrayfr <danrayrollan98@gmail.com>
2024-02-28 19:45:39 +0800

    Update leaderboard design

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -6,12 +6,20 @@ class LeaderboardsController < ApplicationController
   def index
     @skill_total_rankings = Leaderboard.rank_users_by_total_skill_score
     @total_impact_rankings = Leaderboard.rank_users_by_total_impact
+    @skill_rankings_by_category = calculate_skill_rankings_by_category
+    @category = 'total_skill'
+  end
 
-    @skill_per_management = Leaderboard.rank_users_by_skill_total_per_category(:management_skill)
-    @skill_per_technical = Leaderboard.rank_users_by_skill_total_per_category(:technical_skill)
-    @skill_per_communication = Leaderboard.rank_users_by_skill_total_per_category(:communication_skill)
-    @skill_per_financial = Leaderboard.rank_users_by_skill_total_per_category(:financial_skill)
-    @skill_per_analytical = Leaderboard.rank_users_by_skill_total_per_category(:analytical_skill)
-    @skill_per_work_ethics = Leaderboard.rank_users_by_skill_total_per_category(:work_ethics)
+  private 
+
+  def calculate_skill_rankings_by_category
+    # Define an array of skill categories
+    skill_categories = %i[management_skill technical_skill communication_skill
+                          financial_skill analytical_skill work_ethics]
+    skill_rankings_by_category = {}
+    skill_categories.each do |category|
+      skill_rankings_by_category[category] = Leaderboard.rank_users_by_skill_total_per_category(category)
+    end
+    skill_rankings_by_category
   end
 end
--- app/helpers/leaderboards_helper.rb
+++ app/helpers/leaderboards_helper.rb
@@ -1,2 +1,14 @@
 module LeaderboardsHelper
+  def rankings_color(index)
+    case index
+    when 1
+      'bg-gold'
+    when 2
+      'bg-darkStone'
+    when 3
+      'bg-ivory'
+    else
+      'bg-indigo-800'
+    end
+  end
 end
